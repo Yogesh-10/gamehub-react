f5190c675d37ff9e9eac912dcbb39b91a815ccc6
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-03-23 19:50:09 +0530

    build responsive cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 import GameGrid from './components/GameGrid';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <Grid templateAreas={`"nav nav" "aside main"`}>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
       <GridItem area='nav'>
         <NavBar />
       </GridItem>
-      <GridItem area='aside'>Aside</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside'>Aside</GridItem>
+      </Show>
       <GridItem area='main'>
         <GameGrid />
       </GridItem>
