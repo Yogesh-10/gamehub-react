ef7e0a4056ff20573c85ebd7e7b0a1ee4322a31c
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-03-25 12:23:29 +0530

    get optimized cropped images

---
+++ src/services/imageURL.ts
@@ -0,0 +1,13 @@
+//the default size of image coming from API is having more resolution, its okay to show a big image in a full screen. but since
+//we are rendering inside a card, we are reducing resolution by cropping it to 600/400 resolution
+
+//Default URL rawg uses internally to get image - https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
+//We can modify this url to change the crop size by adding crop/600/400 - https://api.rawg.io/media/crop/600/400/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
+
+const getCroppedImageUrl = (url: string) => {
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+};
+
+export default getCroppedImageUrl;
