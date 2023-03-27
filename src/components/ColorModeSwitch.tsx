00de0eaf43dc4eda5c71d9997bc6c8cbb61739c4
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-03-27 15:40:08 +0530

    implement search

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
         onChange={toggleColorMode}
         padding='10px'
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace='nowrap'>Dark Mode</Text>
     </HStack>
   );
 };
--- src/services/imageURL.ts
+++ src/services/imageURL.ts
@@ -3,9 +3,10 @@
 
 //Default URL rawg uses internally to get image - https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
 //We can modify this url to change the crop size by adding crop/600/400 - https://api.rawg.io/media/crop/600/400/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
-
+import noImage from '../assets/no-image-placeholder.webp';
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
+
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
